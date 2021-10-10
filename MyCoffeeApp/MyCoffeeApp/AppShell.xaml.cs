1010c33aa8c03f11bab5ada538506b71acff2af8
SlimBenRomdhane <taf.slim.benromdhane@gmail.com>
2021-10-10 21:10:57 +0100

    Add project files.

---
+++ MyCoffeeApp/MyCoffeeApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using MyCoffeeApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+    }
+}
